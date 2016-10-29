6240809389446c3fd9de09c8e905ae09cb8e2877
Luiz Roseiro <luuroseiro@gmail.com>
2016-10-29 01:00:21 -0200

    Preparando menus

--- Projeto Sidney/main.c
+++ Projeto Sidney/main.c
@@ -6,10 +6,11 @@
 #include <allegro5/allegro_primitives.h>
 #include "objetos.h"
 
-const int WIDTH = 720;
-const int HEIGHT = 560;
-enum KEYS{UP, DOWN, LEFT, RIGHT, SPACE};
-bool keys[5] = {false, false, false, false, false};
+const int WIDTH = 839;
+const int HEIGHT = 685;
+enum KEYS{UP, DOWN, LEFT, RIGHT, SPACE, ESC, P};
+enum STATE{MENUG, MENUC, JOGO, PAUSE};
+bool keys[7] = {false, false, false, false, false, false};
 
 void iniciaAviao(AVIOES *aviao) {
 	aviao->x = WIDTH / 2;
@@ -67,7 +68,7 @@ int main(void) {
 
 	bool done = false, redraw = false, gameOver = false;
 	const int FPS = 60;
-	int destino;
+	int destino, estado = MENUG;
 
 	AVIOES aviao;
 	DESTINOS *pais;
@@ -78,6 +79,15 @@ int main(void) {
 	ALLEGRO_EVENT_QUEUE *fila_de_eventos = NULL;
 	ALLEGRO_TIMER *timer = NULL;
 	ALLEGRO_FONT *fonte = NULL;
+	ALLEGRO_BITMAP *americacn = NULL;
+	ALLEGRO_BITMAP *americas = NULL;
+	ALLEGRO_BITMAP *africa = NULL;
+	ALLEGRO_BITMAP *europa = NULL;
+	ALLEGRO_BITMAP *asia = NULL;
+	ALLEGRO_BITMAP *oceania = NULL;
+	ALLEGRO_BITMAP *menug = NULL;
+	ALLEGRO_BITMAP *menuc = NULL;
+	ALLEGRO_BITMAP *pause = NULL;
 
 	if (!al_init()) {
 		al_show_native_message_box(janela, "ERRO", "Erro ao iniciar Allegro!", NULL, NULL, ALLEGRO_MESSAGEBOX_ERROR);
@@ -89,6 +99,25 @@ int main(void) {
 		al_show_native_message_box(janela, "ERRO", "Erro ao criar janela!", NULL, NULL, ALLEGRO_MESSAGEBOX_ERROR);
 		return -1;
 	}
+
+	americacn = al_load_bitmap("americacn.jpg");
+	americas = al_load_bitmap("americas.jpg");
+	africa = al_load_bitmap("africa.jpg");
+	europa = al_load_bitmap("europa.jpg");
+	asia = al_load_bitmap("asia.jpg");
+	oceania = al_load_bitmap("oceania.jpg");
+	if (!americacn || !americas || !africa || !europa || !asia || !oceania) {
+		al_show_native_message_box(janela, "ERRO", "Erro ao iniciar mapas!", NULL, NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		return -1;
+	}
+
+	/*menug = al_load_bitmap("menug.jpg");
+	menuc = al_load_bitmap("menuc.jpg");
+	pause = al_load_bitmap("pause.jpg");
+	if (!menuc || !menug || !pause) {
+		al_show_native_message_box(janela, "ERRO", "Erro ao iniar telas!", NULL, NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		return -1;
+	}*/
 
 	//addons
 	if (!al_install_keyboard()) {
@@ -165,12 +194,29 @@ int main(void) {
 			if (keys[RIGHT]) {
 				mAviaoDir(&aviao);
 			}
-			redraw = true;
+
+			if (estado == MENUG) {
+
+			}
+			else if (estado == MENUC) {
+
+			}
+			else if (estado == JOGO) {
+				if (keys[ESC] || keys[P]) {
+					estado = PAUSE;
+				}
+			}
+			else if (estado == PAUSE) {
+				if (keys[ESC])
+					done = true;
+			}
+
+			redraw = true; 
 		}
 		else if (evento.type == ALLEGRO_EVENT_KEY_DOWN) {
 			switch (evento.keyboard.keycode) {
 			case ALLEGRO_KEY_ESCAPE:
-				done = true;
+				keys[ESC] = true;
 				break;
 			case ALLEGRO_KEY_UP:
 				keys[UP] = true;
@@ -186,6 +232,9 @@ int main(void) {
 				break;
 			case ALLEGRO_KEY_SPACE:
 				keys[SPACE] = true;
+				break;
+			case ALLEGRO_KEY_P:
+				keys[PAUSE] = true;
 				break;
 			}
 		}
@@ -207,6 +256,12 @@ int main(void) {
 				case ALLEGRO_KEY_SPACE:
 					keys[SPACE] = false;
 					break;
+				case ALLEGRO_KEY_ESCAPE:
+					keys[ESC] = false;
+					break;
+				case ALLEGRO_KEY_P:
+					keys[PAUSE] = false;
+					break;
 				}
 			}
 		}
@@ -214,17 +269,38 @@ int main(void) {
 		if (redraw && al_is_event_queue_empty(fila_de_eventos)) {
 			redraw = false;
 
-			if (!gameOver) {
+			if (estado == MENUG) {
+
+			}
+			else if (estado == MENUC) {
+
+			}
+			else if (estado == JOGO) {
+				/*if (!gameOver) {
 				desenhaAviao(aviao);
-				/*al_draw_textf(fonte, al_map_rgb(255, 255, 255), WIDTH / 2, HEIGHT - 90, 
-					ALLEGRO_ALIGN_CENTRE, "%s", pais->nome);*/
+				al_draw_textf(fonte, al_map_rgb(255, 255, 255), WIDTH / 2, HEIGHT - 90,
+				ALLEGRO_ALIGN_CENTRE, "%s", pais->nome);
+				}*/
+			}
+			else if (estado == PAUSE) {
+				
 			}
 
 			al_flip_display();
 			al_clear_to_color(al_map_rgb(0, 0, 0));
 		}
 	}
-
+	
+	//destruindo
+	al_destroy_bitmap(americacn);
+	al_destroy_bitmap(americas);
+	al_destroy_bitmap(africa);
+	al_destroy_bitmap(europa);
+	al_destroy_bitmap(asia);
+	al_destroy_bitmap(oceania);
+	al_destroy_bitmap(menug);
+	al_destroy_bitmap(menuc);
+	al_destroy_bitmap(pause);
 	al_destroy_event_queue(fila_de_eventos);
 	al_destroy_display(janela);
 
